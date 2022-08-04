44eee00acdc6d47974733770ec1e3e5bc9a498a6
huykieu <kimhuy957@gamil.com>
2022-08-04 23:12:59 +0700

    bai 20update

--- b20.js
+++ b20.js
@@ -1,29 +1,61 @@
-document,addEventListener('DOMContentLoaded',function(){
-    var nut =document.querySelectorAll('.chuyentrang ul li');
-    var slides=this.document.querySelectorAll('.cacslide ul li');
-     console.log(slides);
-    // nut tra ve mot nut 
-    for (var i=0;i<nut.length;i++){
-        nut[i].addEventListener('click',function(){
-            // bo tat ca mau den di 
-            for(var i=0;i<nut.length;i++){
-                nut[i].classList.remove('kichhoat');
-            }
-            this.classList.add('kichhoat');
-            // console.log(this.previousElementSibling);
-            var nutkichhoat=this;
-            var vt=0;
-            for(vt=0;nutkichhoat=nutkichhoat.previousElementSibling;vt++){
-                //  console.log('ben i '+i);
-                //  console.log('phan tu nut kich hoat la '+nutkichhoat);
-            }
-            //het vong lap nay thi thi bien i= so thu tu
-            console.log("vi tri cua phan thu cac class la "+vt);
-            for (var i = 0; i < slides.length; i++) {
-                slides[i].classList.remove('active');
-                slides[vt].classList.add('active');
-            }
-
-        })
+document,
+  addEventListener("DOMContentLoaded", function () {
+    var nut = document.querySelectorAll(".chuyentrang ul li");
+    var slides = this.document.querySelectorAll(".cacslide ul li");
+    console.log(slides);
+    var thoigian = setInterval(function () {autoslide()}, 4000);
+    // nut tra ve mot nut
+    for (var i = 0; i < nut.length; i++) {
+      nut[i].addEventListener("click", function () {
+        // truowc tien phai huy di hieu ung
+        clearInterval(thoigian);
+        // bo tat ca mau den di
+        for (var i = 0; i < nut.length; i++) {
+          nut[i].classList.remove("kichhoat");
+        }
+        this.classList.add("kichhoat");
+        // console.log(this.previousElementSibling);
+        var nutkichhoat = this;
+        var vt = 0;
+        for (vt = 0; (nutkichhoat = nutkichhoat.previousElementSibling); vt++) {
+          //  console.log('ben i '+i);
+          //  console.log('phan tu nut kich hoat la '+nutkichhoat);
+        }
+        //het vong lap nay thi thi bien i= so thu tu
+        console.log("vi tri cua phan thu cac class la " + vt);
+        for (var i = 0; i < slides.length; i++) {
+          slides[i].classList.remove("active");
+          slides[vt].classList.add("active");
+        }
+      });
     }
-})
+    autoslide();
+    function autoslide() {
+      
+        var vitrislide = 0;
+        var slidehientai = document.querySelector(".cacslide ul li.active");
+        // console.log(slidehientai.previousElementSibling);
+        // console.log(slidehientai);
+        for (
+          vitrislide = 0;
+          (slidehientai = slidehientai.previousElementSibling);
+          vitrislide++
+        ) {}
+        if (vitrislide < (slides.length - 1)) {
+          for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+            nut[i].classList.remove("kichhoat");
+          }
+          slides[vitrislide].nextElementSibling.classList.add("active");
+          nut[vitrislide].nextElementSibling.classList.add("kichhoat");
+        } else {
+          for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+            nut[i].classList.remove("kichhoat");
+          }
+          slides[0].classList.add("active");
+          nut[0].classList.add("kichhoat");
+        }
+      
+    }
+  });
